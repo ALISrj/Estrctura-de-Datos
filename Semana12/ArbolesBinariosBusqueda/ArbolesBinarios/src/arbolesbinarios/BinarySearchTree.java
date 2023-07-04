29798e56b9478d9b4377be176c2304436e738a25
Gabriel Rojas <rgrojas4@utpl.edu.ec>
2023-07-03 21:38:53 -0500

    Subida 13

--- Semana12/ArbolesBinariosBusqueda/ArbolesBinarios/src/arbolesbinarios/BinarySearchTree.java
+++ Semana12/ArbolesBinariosBusqueda/ArbolesBinarios/src/arbolesbinarios/BinarySearchTree.java
@@ -27,5 +27,49 @@ public class BinarySearchTree {
     public void add(int value) {
         root = addRecursive(root, value);
     }
+    
+    private Node deleteRecursive(Node current, int value){
+        if (current == null) {
+            return null;
+        }
+        
+        if (value == current.value){
+            // Cuando el nodo es una hoja
+            if (current.left == null && current.right == null) {
+                
+                return null;
+            }
+            // Cuando el nodo tiene un hijo
+            if (current.right == null) { 
+                return current.left;
+            }
+            if (current.left == null) {
+                return current.right;
+            }
+            
+            int smallestValue = findSmallestValue(current.right);
+            current.value = smallestValue;
+            current.right = deleteRecursive(current.right, smallestValue);
+            return current;
+            
+        }
+        
+        if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+        
+        current.right = deleteRecursive(current.right, value);
+        return current;
+    }
+    
+    private int findSmallestValue(Node root){
+        return root.left == null?root.value: findSmallestValue(root.left);
+    }
+    
+    public void delete(int value){
+        root = deleteRecursive(root, value);
+    }
+    
 
 }
